cc1bff220740bb55eead664fb9830dcbdb1bfc2d
ComputerScienceAddict <computergamer844@gmail.com>
2025-07-21 17:22:10 -0700

    google adsense

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,6 +14,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <script 
+          async 
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6739080250708640"
+          crossOrigin="anonymous"
+        />
+      </head>
       <body>{children}</body>
     </html>
   )
